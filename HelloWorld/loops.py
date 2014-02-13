8a3744f6b6a6bd37c245fce5d85dc17a261ee113
William Liu <William.Q.Liu@gmail.com>
2014-02-13 17:20:46 -0500

    Updated with uses of loops

--- HelloWorld/loops.py
+++ HelloWorld/loops.py
@@ -45,6 +45,20 @@ def continue_example():
             continue
         print x
 
+def enumerate_example():
+    """ How to enumerate across a list to get index and value """
+    print "Enumerate Example"
+    for index, value in enumerate(['tic', 'tac', 'toe']):
+        print index, value
+
+def zip_example():
+    """ Go through two lists of equal size with zip """
+    questions = ['name', 'quest', 'favorite color']
+    answers = ['lancelot', 'the holy grail', 'blue']
+    for qst, ans in zip(questions, answers):
+        print "What is your {0}? It is {1}.".format(qst, ans)
+
+
 if __name__ == '__main__':
     """Goes through examples of all the loop variations"""
     for_loop()
@@ -53,4 +67,24 @@ if __name__ == '__main__':
     while_loop(5) # prints 0,1,2,3,4
     break_example() # prints 0,1,2,3,4
     continue_example() # prints 1,3,5,7
+    enumerate_example() # prints 0 tic
+                        #        1 tac
+                        #        2 toe
+    zip_example() # What is your name?  It is lancelot
+                  # What is your quest?  It is the holy grail
+                  # What is your favorite color?  It is blue
 
+    # Loop through a list in reverse
+    for index in reversed(xrange(1, 10, 2)):
+        print index # 9 7 5 3 1
+
+    # To get unique list of items, you can use set()
+    basket = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
+    for f in set(basket):
+        print f # orange, pear, apple, banana
+
+    # Loop through a dict using iteritems() method
+    knights = {'gallahad': 'the pure', 'robin':'the brave'}
+    for index, values in knights.iteritems():
+        print index, values # gallahad the pure
+                            # robin the brave
