f3b7289e1c95754a1a72990c043f534dfac12d04
William Liu <William.Q.Liu@gmail.com>
2014-02-15 16:24:33 -0500

    formatted variables for better names

--- GAClass/counter.py
+++ GAClass/counter.py
@@ -1,45 +1,52 @@
 #!/usr/bin/python
 """ Lesson on piping csv output into python executable """
 # Use in bash to write to output file
-#$cat nytimes.csv | python impressionser.py > mytest.txt
+#$cat nytimes.csv | python counter.py > myoutput.txt
 
 # import required libraries
 import sys
+import re
 
 age = 0
 impressions = 0
 clicks = 0
-maxage = 0
-agetotal = 0
+max_age = 0
+age_total = 0
 total = 0
 
 # Start a counter and store the textfile in memory
 lines = sys.stdin.readlines()
 lines.pop(0)
 
+# Use regular expression to match if numeric, comma, or line return
+mypattern = re.compile(r'[0-9]')
+
 #"Age","Gender","Impressions","Clicks","Signed_In"
 # For each line, find the sum of index 2 in the list.
 for line in lines:
+    
+    result = mypattern.findall(line)
+    print result
+
     cur_age = int(line.strip().split(',')[0]) #Age
     age = age + cur_age
     impressions = impressions + int(line.strip().split(',')[2]) #Impressions
     clicks = clicks + int(line.strip().split(',')[3]) #Clicks
     total = total+1
 
-    # Whose online as a baby?!
+    # Whose online as a baby?!  Ignore babies age 0
     if cur_age > 0:
-        agetotal = agetotal + 1
+        age_total = age_total + 1
     else:
         continue
 
-
     # Store maximum age
-    if cur_age >= maxage:
-        maxage = cur_age
+    if cur_age >= max_age:
+        max_age = cur_age
 
 print "Impressions Sum:", impressions
 print "Age Average:", float(age)/total
-print "Age Average (no zeroes):", float(age)/agetotal
+print "Age Average (no zeroes):", float(age)/age_total
 print "Total Clicks:", clicks
 print "Click through rate:", float(clicks)/impressions
-print "Oldest person in the file:", maxage
+print "Oldest person in the file:", max_age
