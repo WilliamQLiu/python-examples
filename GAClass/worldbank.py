f3eccdf8916ef2a179ba946d9562c1e99aeb523a
William Liu <William.Q.Liu@gmail.com>
2014-02-18 23:56:36 -0500

    Updated World Bank Question

--- GAClass/worldbank.py
+++ GAClass/worldbank.py
@@ -1,14 +1,39 @@
 #!/usr/bin/python
 
 import pandas
+pandas.set_option('expand_frame_repr', False) # expand text on build
+pandas.set_option('display.max_columns', 0) # Display any number of columns
+pandas.set_option('display.width', 0)
+pandas.set_option('max_columns',30)
 
 file_name = 'world_bank.csv'
 
 if __name__ == "__main__":
-    print "Loading File"
+    
+    #print "Loading File"
     try:
-        mydf = pandas.DataFrame.from_csv(file_name)
+        mydf = pandas.DataFrame.from_csv(file_name, header=1, index_col=0)
     except:
         print "Cannot load Data Frame"
     
-    print mydf.head()
+    #print mydf.index # Get list of index
+    #print mydf.columns # Get list of columns
+    print "Original Dataframe", mydf
+
+    #print mydf.describe()
+    #mydf = mydf.dropna(axis=0)
+    #print "DF after dropping NaNs", mydf
+    
+    mydf = mydf.iloc[:29,:22] # Remove rows 30 and lower
+    #mydf.fillna(0, inplace=True)
+    print mydf # Cleaned dataframe
+
+    #print "Print which country has highest value for each indicator type"
+
+    #print mydf.index # Show just index names
+    #print mydf.columns # Show just column names
+    #print mydf.values # Show Values
+    #print mydf.T # Transpose
+    print mydf.idxmax() # Returns index of maximum value
+    #print mydf.describe()
+    print mydf.max() # Returns <class 'pandas.core.series.Series'>
