63e6a7ff0fa8b1a99ce64a5c98edf04d0bed63de
William <William.Q.Liu@gmail.com>
2014-03-11 10:59:58 -0400

    Started Log Examples

---
+++ GAClass/6_logreg/example_logreg.py
@@ -0,0 +1,9 @@
+""" Logistic Regression is a type of probabilistic statistical classification
+ model used to predict a binary response from a binary predictor
+  * We use Linear Regression for a response that is continuous and binary for 
+    one that is 0 and 1's, like alive or dead
+  * Used for predicting the outcome of a categorical dependent variable
+    (i.e., a class label) based on one or more predictor variables (features)
+  * 
+ """
+
