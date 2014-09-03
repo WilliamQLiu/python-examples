aadfc63ef159a67351dc6f23aebaa348a0e2d33f
William Liu <William.Q.Liu@gmail.com>
2014-09-03 17:33:32 -0400

    Added JSON and requests examples

--- JSON/json_sample.py
+++ JSON/json_sample.py
@@ -6,33 +6,77 @@
       An object is an unordered set of name/value pairs.  An object begins with
       { left brace and ends with right brace }.  Each name is followed by : (color)
       and the name/value pairs are separated by , (comma)
+
+      json.tool can be used to validate json from the shell
+      $echo '{"json":"obj"}' | python -mjson.tool
+      {
+          "json": "obj"
+      }
+      $echo '{1.2:3.4}' | python -mjson.tool
+        Returns "Expecting property name enclosed in double quotes
+
+      Tutorial here: http://pymotw.com/2/json/
 """
 
 import json
 
 
+def print_original_object(myobject):
+    """ Helper function to find out what the original object is """
+    print "Original object is type: ", type(myobject)
+    print "Original object is: ", repr(myobject)
+
+
+def print_returned_object(myobject):
+    """ Helpter function to find out what the returned object is """
+    print "Returned object is type: ", type(myobject)
+    print "Returned object is: ", repr(myobject)
+
+
 def encoding_basic_python_object():
+    print "ENCODING BASIC PYTHON OBJECT"
     print "Example 1:"
-    a = json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}])
-    print type(a)  #<type 'str'>
-    print a  # ["foo", {"bar": ["baz", null, 1.0, 2]}]
+    myobject = ['foo', {'bar': ('baz', None, 1.0, 2)}]
+    print_original_object(myobject)
+    #  Original object is type:  <type 'list'>
+    #  Original object is  ['foo', {'bar': ('baz', None, 1.0, 2)}]
+    a = json.dumps(myobject)
+    print_returned_object(myobject)
+    #  Returned object is type:  <type 'list'>
+    #  Returned object is:  ['foo', {'bar': ('baz', None, 1.0, 2)}]
 
     print "Example 2:"
-    b = json.dumps({"c": 0, "b": 0, "a": 0}, sort_keys=True)
-    print type(b)  #<type 'str'>
-    print b  # {"a": 0, "b": 0, "c": 0}
+    myobject = {"c": 0, "b": 0, "a": 0}
+    print_original_object(myobject)
+    #  Original object is type:  <type 'dict'>
+    #  Original object is  {'a': 0, 'c': 0, 'b': 0}
+    b = json.dumps(myobject, sort_keys=True)
+    print_returned_object(myobject)
+    #  Returns type:  <type 'str'>
+    #  Original object is:  {"a": 0, "b": 0, "c": 0}
 
 
 def compact_encoding():
+    print "COMPACT ENCODING"
     print "Example 1:"
-    c = json.dumps([1,2,3,{'4': 5, '6': 7}], separators=(',', ':'))
-    print type(c)  #<type 'str'>
-    print c  # [1,2,3,{"4":5,"6":7}]
+    myobject = [1,2,3,{'4': 5, '6': 7}]
+    print_original_object(myobject)
+    #  Original object is type:  <type 'list'>
+    #  Original object is  [1, 2, 3, {'4': 5, '6': 7}]
+    c = json.dumps(myobject, separators=(',', ':'))
+    print_returned_object(myobject)
+    #  Returned object is type:  <type 'list'>
+    #  Returned object is:  [1, 2, 3, {'4': 5, '6': 7}]
 
 
 def pretty_print():
+    print "PRETTY PRINTING (tldr; use indent to make it look nice"
     print "Example 1:"
-    d = json.dumps({'4': 5, '6': 7}, sort_keys=True,
+    myobject = {'4': 5, '6': 7}
+    print_original_object(myobject)
+    #  Original object is type:  <type 'dict'>
+    #  Original object is  {'4': 5, '6': 7}
+    d = json.dumps(myobject, sort_keys=True,
                    indent=2, separators=(',', ':'))
     print type(d)  #<type 'str'>
     print d
@@ -43,10 +87,16 @@ def pretty_print():
 
 
 def decoding_JSON():
+    print "DECODING JSON"
     print "Example 1:"
-    e = json.loads('["foo", {"bar":["baz", null, 1.0, 2]}]')
-    print type(e)  #<type 'list'>
-    print e  #[u'foo', {u'bar': u['baz', None, 1.0, 2]}]
+    myobject = '["foo", {"bar":["baz", null, 1.0, 2]}]'
+    print_original_object(myobject)
+    #  Original object is type:  <type 'str'>
+    #  Original object is  ["foo", {"bar":["baz", null, 1.0, 2]}]
+    e = json.loads(myobject)
+    print_returned_object(myobject)
+    #  Returned object is type:  <type 'str'>
+    #  Returned object is:  ["foo", {"bar":["baz", null, 1.0, 2]}]
 
 
 #For specializing_JSON_object_decoding() example
@@ -57,14 +107,34 @@ def as_complex(dct):
 
 
 def specializing_JSON_object_decoding():
+    print "Specializing JSON object decoding"
     print "Example 1:"
-    print json.loads('{"__complex__": true, "real": 1, "imag": 2}',
+    myobject = '{"__complex__": true, "real": 1, "imag": 2}'
+    print_original_object(myobject)
+    #  Original object is type:  <type 'str'>
+    #  Original object is  {"__complex__": true, "real": 1, "imag": 2}
+    f = json.loads(myobject,
                  object_hook=as_complex)  #(1+2j)
+    print_returned_object(myobject)
+    #  Returned object is type:  <type 'str'>
+    #  Returned object is:  {"__complex__": true, "real": 1, "imag": 2}
+
+
+def dumped_length():
+    """ Shows different file sizes of each """
+    print "DUMPED LENGTH"
+    data = [ { 'a':'A', 'b':(2, 4), 'c':3.0 } ]
+    print 'DATA:', repr(data)  # DATA: [{'a': 'A', 'c': 3.0, 'b': (2, 4)}]
+    print 'repr(data)             :', len(repr(data))  # 35
+    print 'dumps(data)            :', len(json.dumps(data))  # 35
+    print 'dumps(data, indent=2)  :', len(json.dumps(data, indent=2))  # 76
+    print 'dumps(data, separators):', len(json.dumps(data, separators=(',',':')))  # 29
 
 
 if __name__ == '__main__':
-    encoding_basic_python_object()
-    compact_encoding()
-    pretty_print()
+    #encoding_basic_python_object()
+    #compact_encoding()
+    #pretty_print()
     decoding_JSON()
-    specializing_JSON_object_decoding()
+    #specializing_JSON_object_decoding()
+    #dumped_length()
--- requests/example_requests.py
+++ requests/example_requests.py
@@ -1,12 +1,82 @@
 """ Using Python's requests library """
 
+import json
 import requests
 
 
-def get_webpage(site):
-    """ GET """
+def get_webpage_details(site):
+    """ GET details of a request """
     r = requests.get(site)
+    print "Status Code: ", r.status_code
+    #print r.headers  # Gets all headers as a dict
+    print "Get specific field (e.g. 'content-type'):", r.headers['content-type']  # Get specific field
+    print "Get encoding: ", r.encoding
+    #print "Get Text: ", r.text  # Get all text of page
+    print "Get JSON: ", r.json()  #
+    print "GET Response Code: ", r.status_code
+
+
+def request_API_calls():
+    """ Using all HTTP request types (POST, PUT, DELETE, HEAD, OPTIONS) """
+    r = requests.post('http://httpbin.org/post')  # Example of POST
+    print "POST: ", r
+    r = requests.put('http://httpbin.org/put')  # Example of PUT
+    print "PUT: ", r
+    r = requests.delete('http://httpbin.org/delete')  # Example of DELETE
+    print "DELETE: ", r
+    r = requests.head('http://httpbin.org/get')  # Example of HEAD
+    print "HEAD: ", r
+    r = requests.options('http://httpbin.org/get')  # Example of OPTIONS
+    print "OPTIONS: ", r
+
+
+def pass_params_in_urls():
+    """ How to pass data in the URL's query string
+        By hand, getting URL would be given as key/value pairs in the URL
+        after the question mark (e.g. httpbin.org/get?key=val)
+    """
+
+    # If you want to pass 'key1=value1' and 'key2=value2' to 'httpbin.org/get'
+    payload = {'key1': 'value1', 'key2': 'value2'}
+    r = requests.get("http://httpbin.org/get", params=payload)
+
+    # Verify that URL has been encoded correctly by printing out URL
+    print "URL is: ", r.url  # http://httpbin.org/get?key2=value2&key1=value1
+
+
+def pass_headers_in_request():
+    """ Add HTTP headers to a request by adding a dict to the 'headers' param """
+    url = 'https://api.github.com/some/endpoint'
+    payload = { 'some': 'data' }
+    headers = { 'content-type': 'application/json' }
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
+    print r
+
+
+def response_content():
+    """ We can read the server's response """
+    r = requests.get('https://developer.github.com/v3/activity/events/#list-public-events')
+    print "Server's Response is: ", r.text
+
+    # When you make a request, Requests makes an educated guess on encoding
+    # based on the response of the HTTP headers
+    print "Guessed encoding is: ", r.encoding  # utf-8
+    #print "Peak at content if unsure of encoding, sometimes specified in here ", r.content
+
+
+def json_response_content():
+    """ There's a builtin JSON decoder for dealing with JSON data """
+    r = requests.get('http://www.json-generator.com/api/json/get/bVVKnZVjpK?indent=2')
+    print "Getting JSON: ", r  # Should be 200 or else if error, then 401 (Unauthorized)
+    #print r.json()
 
 
 if __name__ == '__main__':
-    get_webpage('https://api.github.com/events')
+    #get_webpage_details('https://api.github.com/events')
+    #request_API_calls()
+    #pass_params_in_urls()
+    pass_headers_in_request()
+    #response_content()
+    #json_response_content()
+
+
