0094e21046854713038132152690337bbb8041b3
Will <William.Q.Liu@gmail.com>
2015-02-02 17:04:41 -0500

    Updated class example

--- std_lib/classobjects.py
+++ std_lib/classobjects.py
@@ -1,29 +1,75 @@
-""" How to use Python's Class and Object"""
-# Class is CarClass(), which is basically a template
-# car1 and car2 are Objects from Class CarClass()
+"""
+    How to use Python's Class
+
+    First example is about creating a class,then creating instances from class
+    Class is CarClass(), which is basically a template
+    car1 and car2 are Objects from Class CarClass()
+
+    Note that a class doesn't necessarily need 'self', can be any variable
+    * _singleleadingunderscore means a weak 'internal use' indicator (when
+        class is imported, the objects that start with an _ are not imported)
+    * singletrailingunderscore_ means to avoid a conflict with Python keyword
+        (e.g. class is now class_)
+    * __doubleleadingunderscore means naming a class attribute
+    * __doubleleadandtrailunderscore__ means 'magic' objects or attributes;
+        don't create these, just use as documented (e.g, __init__, __doc__)
+
+    Docs: https://docs.python.org/2/tutorial/classes.html
+
+    To Do, learn about:
+        inheritance
+        class attributes
+        __dict__
+        subclassing built in types
+        __new__
+        __getattr__ and __setattr__
+        private attributes (single and double underscore)
+        classmethods and staticmethods
+
+"""
+
 
 class CarClass():
     """ The template for cars, has own attributes """
-    cartype="stuff"
+    cartype = "stuff"
     name = "blah"
     worth = 1000
+
     def report_back(self):
         print "Car type is: ", self.cartype
         print "Car name is: ", self.name
         print "Car worth is: ", self.worth, "\n"
 
+
+class Greeting(object):
+    """ Example to see what __init__ and methods do """
+    def __init__(self):
+        self.message = "Hello"
+
+    def my_method(self, name):
+        # When a method is called from a Class,
+        # __init__ is the first argment passed (self)
+        print self.message + " " + name
+
+
 if __name__ == '__main__':
 
     # Create first object, then assign attributes
     car1 = CarClass()
-    car1.cartype="red convertible"
-    car1.name="Fer"
-    car1.worth=60000
+    car1.cartype = "red convertible"
+    car1.name = "Fer"
+    car1.worth = 60000
     car1.report_back()
 
     # Create second object, then assign attributes
     car2 = CarClass()
-    car2.cartype="blue van"
-    car2.name="Jump"
-    car2.worth=10000
+    car2.cartype = "blue van"
+    car2.name = "Jump"
+    car2.worth = 10000
     car2.report_back()
+
+    # Greeting example
+    a = Greeting()  # We do not pass any argument to the __init__ method
+    a.my_method("William!")  # Only pass a single argument # 'Hello William!'
+    # In our 'my_method', there's actually two arguments passed;
+    # 1st arg is always 'self' (from __init__) and 2nd arg is 'William'
